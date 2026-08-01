9ed5e572e051ae8a4b563f2f489d5d96dde1eb8a
fenilxpatel1234-ctrl <fenilxpatel1234-ctrl@users.noreply.github.com>
2026-08-01 19:59:17 +0530

    Update veneers, whitening, implant, cancer screening content

--- server.ts
+++ server.ts
@@ -214,6 +214,24 @@ function sendNewAppointmentNotification(apt: AppointmentRequest): void {
   deliverEmail(apt.email, `We Received Your First Avenue Dentistry Appointment Request`, body);
 }
 
+// --- SEO ---
+app.get('/robots.txt', (req: Request, res: Response) => {
+  res.type('text/plain').send('User-agent: *\nAllow: /\nSitemap: https://firstavenuedentistry.com/sitemap.xml');
+});
+app.get('/sitemap.xml', (req: Request, res: Response) => {
+  res.type('application/xml').send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>https://firstavenuedentistry.com/</loc><priority>1.0</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#home</loc><priority>0.9</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#book-online</loc><priority>0.8</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#emergency</loc><priority>0.8</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#our-team</loc><priority>0.7</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#contact-us</loc><priority>0.7</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#blog</loc><priority>0.6</priority></url>
+  <url><loc>https://firstavenuedentistry.com/#legal</loc><priority>0.5</priority></url>
+</urlset>`);
+});
+
 // --- API ENDPOINTS ---
 
 // Health Check
